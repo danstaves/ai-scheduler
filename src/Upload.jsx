8cf9c86c4eed86ed68668d0198aa5a485136b56a
Dan <danstaves@gmail.com>
2025-03-19 19:10:52 -0400

    create upload page

--- src/Upload.jsx
+++ src/Upload.jsx
@@ -1,25 +1,41 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import style from './Upload.module.css';
 
 function Upload(){
-    const [fileName, setFileName] = useState("No File Selected");
+    const [file, setFile] = useState(null);
+    const fileName = useMemo(() => file ? file.name : "No File Selected", [file]);
+    const [numClasses, setNumClasses] = useState(4);
+
     function onSelectFileClick(){
         const input = document.createElement('input');
         input.type = 'file';
         input.onchange = e => {
             const file = e.target.files[0];
-            setFileName(file.name);
+            setFile(file);
         }
         input.click();
     }
+
+    function onSetNumClasses(e){
+        const newNum = e.target.value;
+        if(/[\d]+/.test(newNum)) setNumClasses(newNum);
+    }
+
     return (
         <div className={style.Upload}>
             <h1>Let us plan your next Semester</h1>
             <p><span style={{fontWeight:"bold"}}>Upload your Degree Evaluation</span> to get a personalized recommendation.</p>
-            <div className={style.uploadButton}>
+            <div className={style.selectButton}>
                 <button onClick={onSelectFileClick}>↗ Select File</button>
                 <p>{fileName}</p>
             </div>
+
+            {file && <div className={style.creditHours}>
+                <p>How many classes would you like to take?</p>
+                <input type="number" min="1" max="20" value={numClasses} onChange={onSetNumClasses}/>
+            </div>}
+
+            {file && <button className={style.uploadButton}>Upload</button>}
         </div>
     );
 }
